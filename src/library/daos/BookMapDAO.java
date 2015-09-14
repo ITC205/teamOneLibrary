6b323b09ad07d136451e5dd1f36cd413ec9a2f62
Josh <josh-kent@hotmail.com>
2015-09-14 13:07:34 +1000

    Implemented variables, constructor and commented section headings

--- src/library/daos/BookMapDAO.java
+++ src/library/daos/BookMapDAO.java
@@ -1,8 +1,10 @@
 package library.daos;
 
 import java.util.List;
+import java.util.Map;
 
 import library.interfaces.daos.IBookDAO;
+import library.interfaces.daos.IBookHelper;
 import library.interfaces.entities.IBook;
 
 /**
@@ -12,7 +14,40 @@ import library.interfaces.entities.IBook;
  */
 public class BookMapDAO implements IBookDAO
 {
+  // ==========================================================================
+  // Variables
+  // ==========================================================================
+  
+  
+  
+  private int nextId_;
+  private Map<Integer, IBook> bookMap_;
+  private IBookHelper helper_;
+  
+  
+  
+  // ==========================================================================
+  // Constructor
+  // ==========================================================================
+  
+  
+  
+  public BookMapDAO(IBookHelper helper) {
+    if(helper == null) {
+      throw new IllegalArgumentException("BookMapDAO: constructor: value for "
+                                         + "'helper' cannot be null");
+    }
+    helper_ = helper;
+  }
+  
+  
+  
+  // ==========================================================================
+  // Insertion Methods
+  // ==========================================================================
 
+  
+  
   @Override
   public IBook addBook(String author, String title, String callNo)
   {
@@ -20,8 +55,14 @@ public class BookMapDAO implements IBookDAO
     return null;
   }
 
+  
 
-
+  // ==========================================================================
+  // Retrieval Methods
+  // ==========================================================================
+  
+  
+  
   @Override
   public IBook getBookByID(int id)
   {
