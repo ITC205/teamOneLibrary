9787cc2eef021119081408459abd7cd8427d7a09
nickbaldwin <nick.with.a.z@gmail.com>
2015-09-17 12:34:19 +1000

    TestSuite for Loan family added

---
+++ src/test/unit/TestSuiteLoanFamily.java
@@ -0,0 +1,16 @@
+package test.unit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Groups test classes to be run as a suite.
+ */
+@RunWith(org.junit.runners.Suite.class)
+@SuiteClasses({TestLoan.class})
+
+public class TestSuiteLoanFamily
+{
+ /* empty class */
+}
