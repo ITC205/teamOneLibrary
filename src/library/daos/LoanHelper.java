469f0bedd7b04862cd2e1846442357d25e78779b
nickbaldwin <nick.with.a.z@gmail.com>
2015-09-21 16:08:19 +1000

    LoanHelper: make() to support initial tests

--- src/library/daos/LoanHelper.java
+++ src/library/daos/LoanHelper.java
@@ -1,29 +1,46 @@
 package library.daos;
 
+import java.util.Date;
+
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+import library.interfaces.daos.ILoanHelper;
+
+import library.entities.Loan;
+
 /**
- *
+ * Creates a new Loan instance.
  */
 public class LoanHelper
+  implements ILoanHelper
 {
   //===========================================================================
   // Variables
   //===========================================================================
 
+  private final int DEFAULT_ID = 0; // new items have id of zero by default
 
   //===========================================================================
   // Constructors
   //===========================================================================
 
-  private LoanHelper()
-
+  public LoanHelper()
   {
   }
-
 
   //===========================================================================
   // Primary methods
   //===========================================================================
 
+  @Override
+  public ILoan makeLoan(IBook book, IMember borrower,
+                        Date borrowDate, Date dueDate)
+  {
+    return new Loan(book, borrower,
+                    borrowDate, dueDate,
+                    DEFAULT_ID);
+  }
 
   //===========================================================================
   // Helper methods
