ac083fd05a2e07052ed3341d20f4a3c02f0ea035
nickbaldwin <nick.with.a.z@gmail.com>
2015-09-21 16:07:39 +1000

    TestLoanHelper: + initial tests new Loan

--- src/test/unit/TestLoanHelper.java
+++ src/test/unit/TestLoanHelper.java
@@ -1,7 +1,24 @@
 package test.unit;
 
+import java.util.Date;
+
+import java.lang.reflect.Constructor;
+
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.IMember;
+import library.interfaces.entities.ILoan;
+
+import library.daos.LoanHelper;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import static test.unit.TestLoanBuilder.*;
+
 /**
- *
+ * Factory class for producing new
  */
 public class TestLoanHelper
 {
@@ -10,29 +27,86 @@ public class TestLoanHelper
   //===========================================================================
 
 
+
   //===========================================================================
-  // Constructors
+  // Primary methods testing - with stubs & dateBuilder (from TestLoanBuilder)
   //===========================================================================
 
-  private TestLoanHelper()
-
+  @Test
+  public void makeLoan()
   {
+    LoanHelper helper = new LoanHelper();
+
+    IBook book = stubBook();
+    IMember borrower = stubMember();
+    Date borrowDate = dateBuilder(20, 11, 2015);
+    Date dueDate = dateBuilder(31, 11, 2015);
+
+    ILoan loan = helper.makeLoan(book, borrower, borrowDate, dueDate);
+
+    // Then loan should be instantiated
+    assertThat(loan).isInstanceOf(ILoan.class);
   }
 
 
-  //===========================================================================
-  // Primary methods
-  //===========================================================================
+  @Test
+  public void makeLoanHasDefaultIdZero()
+  {
+    LoanHelper helper = new LoanHelper();
+
+    IBook book = stubBook();
+    IMember borrower = stubMember();
+    Date borrowDate = dateBuilder(20, 11, 2015);
+    Date dueDate = dateBuilder(31, 11, 2015);
+
+    ILoan loan = helper.makeLoan(book, borrower, borrowDate, dueDate);
+
+    // Then loan should have default ID of zero
+    assertThat(loan.getID()).isEqualTo(0);
+  }
 
 
-  //===========================================================================
-  // Helper methods
-  //===========================================================================
+  @Test
+  public void makeLoanIsFactoryForCreatingUniqueLoans()
+  {
+    LoanHelper helper = new LoanHelper();
+
+    IBook book = stubBook();
+    IMember borrower = stubMember();
+    Date borrowDate = dateBuilder(20, 11, 2015);
+    Date dueDate = dateBuilder(31, 11, 2015);
+
+    ILoan firstLoan = helper.makeLoan(book, borrower, borrowDate, dueDate);
+    ILoan secondLoan = helper.makeLoan(book, borrower, borrowDate, dueDate);
+    ILoan thirdLoan = helper.makeLoan(book, borrower, borrowDate, dueDate);
+
+    // Then loans are all unique instances (even if properties are the same)
+    assertThat(firstLoan).isNotSameAs(secondLoan);
+    assertThat(firstLoan).isNotSameAs(thirdLoan);
+  }
 
 
-  //===========================================================================
-  // Getters & setters
-  //===========================================================================
 
+
+  // TODO: check if LoanHelper constructor to be private & thus need this method
+  /*
+   * Uses Reflection API to directly access LoanHelper's private constructor.
+   */
+  private LoanHelper createLoanHelperWithPrivateConstructor()
+  {
+    try {
+      Constructor constructor = LoanHelper.class.getConstructor(String.class);
+      LoanHelper loanHelper = (LoanHelper)constructor.newInstance();
+      return loanHelper;
+    }
+
+    catch (IllegalAccessException exception) {
+      fail("IllegalAccessException should not occur");
+    }
+    catch (Exception exception) {
+      fail("Exception should not occur");
+    }
+    return null;
+  }
 
 }
