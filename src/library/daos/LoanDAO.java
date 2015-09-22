c95ca863ade01d7b8d33c0e166d841272a9797d5
nickbaldwin <nick.with.a.z@gmail.com>
2015-09-22 16:21:16 +1000

    LoanDAO: + C & helper

--- src/library/daos/LoanDAO.java
+++ src/library/daos/LoanDAO.java
@@ -1,11 +1,15 @@
 package library.daos;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.IMember;
 import library.interfaces.entities.ILoan;
-import library.interfaces.entities.ELoanState;
 
 import library.interfaces.daos.ILoanHelper;
+
+import library.entities.Loan;
 
 /**
  * Manages all Loans within the system, from creation of pending loans to
@@ -14,12 +18,14 @@ import library.interfaces.daos.ILoanHelper;
  * loans.
  */
 public class LoanDAO
+//  implements ILoanDAO
 {
   //===========================================================================
   // Variables
   //===========================================================================
 
   private ILoanHelper helper_;
+  private Calendar calendar_ = Calendar.getInstance();
 
   //===========================================================================
   // Constructors
@@ -44,7 +50,37 @@ public class LoanDAO
   // Primary methods
   //===========================================================================
 
+  /*
+   * Uses LoanHelper to create a new Loan with default id of zero, sets
+   * borrow date on current system time and sets due date using the standard
+   * loan duration (from borrow date).
+   * @param borrower IMember The borrower who wishes to create a loan.
+   * @param book IBook The book that the borrower wishes to loan.
+   * @return Loan A pending loan associated with the borrower and the book,
+   * with and id of zero.
+   * throws IllegalArgumentException if borrower or book is null.
+   */
+  // @Override
+  public void createLoan(IBook book, IMember borrower)
+  {
+    // throwIfObjectNull("Book", book);
+    // throwIfObjectNull("Borrower", borrower);
+    Date borrowDate = ignoreTime(new Date());
+    Date dueDate = calculateDueDate(borrowDate);
+    // Date dueDate = calculateDueDate(borrowDate);
 
+    helper_.makeLoan(book, borrower, borrowDate, dueDate);
+  }
+
+
+  private <T> void throwIfObjectNull(String parameterName, T object)
+      throws IllegalArgumentException
+  {
+    String message = "Cannot create a new Loan with a null ";
+    if (object == null) {
+      throw new IllegalArgumentException(message + parameterName);
+    }
+  }
 
   //===========================================================================
   // Helper methods
@@ -55,5 +91,29 @@ public class LoanDAO
   // Getters & setters
   //===========================================================================
 
+
+
+
+  private Date ignoreTime(Date date)
+  {
+    java.util.Calendar calendar = java.util.Calendar.getInstance();
+
+    calendar.setTime(date);
+    calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);
+    calendar.set(java.util.Calendar.MINUTE, 0);
+    calendar.set(java.util.Calendar.SECOND, 0);
+    calendar.set(java.util.Calendar.MILLISECOND, 0);
+
+    return calendar.getTime();
+  }
+
+
+
+  private Date calculateDueDate(Date borrowDate)
+  {
+    calendar_.setTime(borrowDate);
+    calendar_.add(java.util.Calendar.DATE, ILoan.LOAN_PERIOD);
+    return calendar_.getTime();
+  }
 
 }
