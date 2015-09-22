4a4fb1aef27810d75472fcbf0d6a1617de99ad25
nickbaldwin <nick.with.a.z@gmail.com>
2015-09-22 12:18:56 +1000

    LoanDAO: + getInstance()

--- src/library/daos/LoanDAO.java
+++ src/library/daos/LoanDAO.java
@@ -1,7 +1,10 @@
 package library.daos;
 
 /**
- *
+ * Manages all Loans within the system, from creation of pending loans to
+ * committing loans to 'persistence' and provides methods for finding
+ * individual loans, identifying overdue loans and updating state of overdue
+ * loans.
  */
 public class LoanDAO
 {
@@ -9,21 +12,27 @@ public class LoanDAO
   // Variables
   //===========================================================================
 
+  private static LoanDAO instance_ = null;
 
   //===========================================================================
   // Constructors
   //===========================================================================
 
   private LoanDAO()
-
   {
   }
-
 
   //===========================================================================
   // Primary methods
   //===========================================================================
 
+  public static LoanDAO getInstance()
+  {
+    if (instance_ == null) {
+      instance_ = new LoanDAO();
+    }
+    return instance_;
+  }
 
   //===========================================================================
   // Helper methods
