7a9aae30de190ea9954c9b65ce4b3481172154da
nickbaldwin <nick.with.a.z@gmail.com>
2015-09-24 09:14:50 +1000

    LoanHelper: removed remaining reflection use

--- src/test/unit/TestLoanHelper.java
+++ src/test/unit/TestLoanHelper.java
@@ -32,7 +32,7 @@ public class TestLoanHelper
   @Test
   public void makeLoan()
   {
-    LoanHelper helper = createLoanHelperWithProtectedConstructor();
+    LoanHelper helper = new LoanHelper();
 
     IBook book = stubBook();
     IMember borrower = stubMember();
@@ -49,7 +49,7 @@ public class TestLoanHelper
   @Test
   public void makeLoanSetsExpectedProperties()
   {
-    LoanHelper helper = createLoanHelperWithProtectedConstructor();
+    LoanHelper helper = new LoanHelper();
 
     IBook book = stubBook();
     IMember borrower = stubMember();
