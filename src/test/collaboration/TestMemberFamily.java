abeff4f46b28d16741edfab51d1cec8d75afc106
rcallow <rcallow87@gmail.com>
2015-09-25 04:05:45 +1000

    Continued implementing TestMemberFamily

--- src/test/collaboration/TestMemberFamily.java
+++ src/test/collaboration/TestMemberFamily.java
@@ -1,5 +1,90 @@
 package test.collaboration;
 
-public class TestMemberFamily {
+import junit.framework.TestCase;
+import library.entities.Member;
+import library.interfaces.entities.*;
+import static org.mockito.Mockito.*;
 
+
+/**
+* The TestMember class implements unit testing
+* on methods contained within the Member class
+*
+* @author  Rebecca Callow
+*/
+public class TestMemberFamily extends TestCase
+{
+	
+	public void createMembers()
+	{
+		String[] firstNames = {"Sam","Joe","Bill","Sarah","Jodie","Samantha"};
+		String[] lastNames = {"Jones","Jenkins","Bloggs","Doe","Woods","Cook"};
+		String[] contactPhoneNumbers = {"123456","1","321","0412123123","55555555","12341234"};
+		String[] emailAddresses = {"sjones@mail.com","jjenkins@email.com","bbloggs@email.com",
+				                   "sdoe@email.com","jwoods@email.com","scook@email.com"};
+		
+		
+	}
+	
+	  public void testMemberDAOValid()
+	  {
+		  
+	  }
+	  
+	  
+	  
+	  public void testAddMember()
+	  {
+		  
+	  }
+	  
+	  
+	  
+	  public void testGetMemberByIdValid()
+	  {
+		  
+	  }
+	  
+	  
+	  
+	  public void testGetMemberByIdInvalid()
+	  {
+		  
+	  }
+	  
+	  
+	  
+	  public void testListMembers()
+	  {
+		  
+	  }
+	  
+	  
+	  
+	  public void testFindMembersByLastName()
+	  {
+		  
+	  }
+	  
+	  
+	  
+	  public void testFindMembersByLastNameDoesNotExist()
+	  {
+		  
+	  }
+	  
+	  
+	  
+	  public void testFindMembersByEmailAddress()
+	  {
+		  
+	  }
+	  
+	  
+	  
+	  public void testFindMembersByNames()
+	  {
+		  
+	  }
+	
 }
