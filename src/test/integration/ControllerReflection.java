bf3b822711a3c32cc225980b3bbf7b306fc94b25
nickbaldwin <nick.with.a.z@gmail.com>
2015-09-26 11:37:38 +1000

    confirm Loans: first happy path tests & helpers

--- src/test/integration/ControllerReflection.java
+++ src/test/integration/ControllerReflection.java
@@ -1,5 +1,6 @@
 package test.integration;
 
+import java.util.List;
 import java.util.Date;
 import java.util.Map;
 
@@ -9,7 +10,15 @@ import java.lang.reflect.InvocationTargetException;
 
 import library.BorrowUC_CTL;
 
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+
 import  library.interfaces.EBorrowState;
+
+
+import library.entities.Loan;
+
+
 
 import static org.assertj.core.api.Assertions.fail;
 
@@ -20,15 +29,14 @@ import static org.assertj.core.api.Assertions.fail;
 public class ControllerReflection
 {
   //===========================================================================
-  // State getter
+  // State getter & setter
   //===========================================================================
 
   /*
-   * Uses Reflection API to directly access BorrowUC_CTL's private state and
-   * return the current state of the controller.
-   * @param controller BorrowUC_CTL The main controller.
-   * @return EBorrowState The current state of the controller.
-   */
+ * Uses Reflection API to directly access the BorrowUC_CTL's private state.
+ * @param controller BorrowUC_CTL The main controller.
+ * @return EBorrowState The current state of the controller.
+ */
   public static EBorrowState getPrivateState(BorrowUC_CTL controller)
   {
     try {
@@ -52,20 +60,148 @@ public class ControllerReflection
     catch (Exception exception) {
       fail("Exception should not occur");
     }
+
     return null;
   }
 
-  //===========================================================================
-  // ?
-  //===========================================================================
 
 
+  /*
+   * Uses Reflection API to directly set BorrowUC_CTL's private state.
+   * @param controller BorrowUC_CTL The main controller.
+   * @param newState EBorrowState The new state to be set on the controller.
+   */
+  public static void setPrivateState(BorrowUC_CTL controller, EBorrowState newState)
+  {
+    try {
+      Class<?> controllerClass = controller.getClass();
+      Field state = controllerClass.getDeclaredField("state");
 
-  //===========================================================================
-  // ?
-  //===========================================================================
+      // Enable direct modification of private field
+      if (!state.isAccessible()) {
+        state.setAccessible(true);
+      }
 
+      state.set(controller, newState);
+    }
 
-
+    catch (NoSuchFieldException exception) {
+      fail("NoSuchFieldException should not occur");
+    }
+    catch (IllegalAccessException exception) {
+      fail("IllegalAccessException should not occur");
+    }
+    catch (Exception exception) {
+      fail("Exception should not occur");
+    }
   }
+
+
+
+
+  //===========================================================================
+  // ?
+  //===========================================================================
+
+  /*
+ * Uses Reflection API to directly set BorrowUC_CTL's private loanList.
+ * @param controller BorrowUC_CTL The main controller.
+ * @param pendingLoans List<ILoan> The list of pending loans to be set.
+ */
+  public static List<ILoan> getPrivateLoanList(BorrowUC_CTL controller)
+  {
+    try {
+      Field loanList = BorrowUC_CTL.class.getDeclaredField("loanList");
+
+      // Enable direct modification of private field
+      if (!loanList.isAccessible()) {
+        loanList.setAccessible(true);
+      }
+
+      return (List<ILoan>)loanList.get(controller);
+    }
+
+    catch (NoSuchFieldException exception) {
+      fail("NoSuchFieldException should not occur");
+    }
+    catch (IllegalAccessException exception) {
+      fail("IllegalAccessException should not occur");
+    }
+    catch (Exception exception) {
+      fail("Exception should not occur");
+    }
+
+    return null;
+  }
+
+
+
+  /*
+   * Uses Reflection API to directly set BorrowUC_CTL's private loanList.
+   * @param controller BorrowUC_CTL The main controller.
+   * @param pendingLoans List<ILoan> The list of pending loans to be set.
+   */
+  public static void setPrivateLoanList(BorrowUC_CTL controller, List<ILoan> pendingLoans)
+  {
+    try {
+      Field loanList = BorrowUC_CTL.class.getDeclaredField("loanList");
+
+      // Enable direct modification of private field
+      if (!loanList.isAccessible()) {
+        loanList.setAccessible(true);
+      }
+
+      loanList.set(controller, pendingLoans);
+    }
+
+    catch (NoSuchFieldException exception) {
+      fail("NoSuchFieldException should not occur");
+    }
+    catch (IllegalAccessException exception) {
+      fail("IllegalAccessException should not occur");
+    }
+    catch (Exception exception) {
+      fail("Exception should not occur");
+    }
+  }
+
+
+  //===========================================================================
+  // State setter
+  //===========================================================================
+
+  /*
+ * Uses Reflection API to directly access the BorrowUC_CTL's private borrower.
+ * @param controller BorrowUC_CTL The main controller.
+ * @return IMember The current borrower set in the controller.
+ */
+  public static IMember getPrivateBorrower(BorrowUC_CTL controller)
+  {
+    try {
+      Field borrower = BorrowUC_CTL.class.getDeclaredField("borrower");
+
+      // Enable direct modification of private field
+      if (!borrower.isAccessible()) {
+        borrower.setAccessible(true);
+      }
+
+      return (IMember)borrower.get(controller);
+    }
+
+    catch (NoSuchFieldException exception) {
+      fail("NoSuchFieldException should not occur");
+    }
+    catch (IllegalAccessException exception) {
+      fail("IllegalAccessException should not occur");
+    }
+    catch (Exception exception) {
+      fail("Exception should not occur");
+    }
+
+    return null;
+  }
+
+
+
+}
 
--- src/test/integration/TestConfirmLoansOperation.java
+++ src/test/integration/TestConfirmLoansOperation.java
@@ -1,6 +1,10 @@
 package test.integration;
 
 import java.util.List;
+import java.util.ArrayList;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import library.interfaces.hardware.ICardReader;
 import library.interfaces.hardware.IScanner;
@@ -30,6 +34,9 @@ import library.entities.Book;
 import library.entities.Loan;
 import library.entities.Member;
 
+import library.BorrowUC_CTL;
+import library.interfaces.EBorrowState;
+
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -37,6 +44,7 @@ import static org.mockito.Mockito.*;
 
 import static test.unit.LoanBuilder.*;
 import static test.unit.LoanReflection.*;
+import static test.integration.ControllerReflection.*;
 
 
 /**
@@ -62,6 +70,8 @@ public class TestConfirmLoansOperation
   // Fixtures
   //===========================================================================
 
+  library.BorrowUC_CTL controller;
+
   ICardReader reader = mock(ICardReader.class);
   IScanner scanner = mock(IScanner.class);
   IPrinter printer = mock(IPrinter.class);
@@ -76,25 +86,40 @@ public class TestConfirmLoansOperation
   IMemberHelper memberHelper = new MemberHelper();
   IMemberDAO members = new MemberDAO(memberHelper);
 
-  IBook catch22 = books.addBook("CATCH-22", "Joseph Heller", "101.1 [2]");
-  IBook emma = books.addBook("Emma", "Jane Austen", "102.5");
-  IBook scoop = books.addBook("Scoop", "Evelyn Waugh", "103.21");
-  IBook dune = books.addBook("Dune", "Frank Herbert", "104 [21]");
-  IBook janeEyre = books.addBook("Jane Eyre", "Charlotte Brontë", "105.2");
-  IBook animalFarm = books.addBook("Animal Farm", "George Orwell", "106 [1]");
-  IBook ulysses = books.addBook("Ulysses", "James Joyce", "107.345");
-  IBook onTheRoad = books.addBook("On the Road", "Jack Kerouac", "108.1");
-  IBook dracula = books.addBook("Dracula", "Bram Stoker", "109.1 [21]");
-  IBook middlemarch = books.addBook("Middlemarch", "George Eliot", "110");
-  IBook hobbit = books.addBook("The Hobbit", "J.R.R. Tolkien", "111.23");
-  IBook atonement = books.addBook("Atonement", "Ian McEwan", "112.2 [22]");
-  IBook iClaudius = books.addBook("I, Claudius", "Robert Graves", "113 [2]");
+  IBook catch22 = books.addBook("Joseph Heller", "CATCH-22", "101.1 [2]");
+  IBook emma = books.addBook("Jane Austen", "Emma", "102.5");
+  IBook scoop = books.addBook("Evelyn Waugh", "Scoop", "103.21");
+  IBook dune = books.addBook("Frank Herbert", "Dune", "104 [21]");
+  IBook janeEyre = books.addBook("Charlotte Brontë", "Jane Eyre", "105.2");
+  IBook animalFarm = books.addBook("George Orwell", "Animal Farm", "106 [1]");
+  IBook ulysses = books.addBook("James Joyce", "Ulysses", "107.345");
+  IBook onTheRoad = books.addBook("Jack Kerouac", "On the Road", "108.1");
+  IBook dracula = books.addBook("Bram Stoker", "Dracula", "109.1 [21]");
+  IBook middlemarch = books.addBook("George Eliot", "Middlemarch", "110");
+  IBook hobbit = books.addBook("J.R.R. Tolkien", "The Hobbit", "111.23");
+  IBook atonement = books.addBook("Ian McEwan", "Atonement", "112.2 [22]");
+  IBook iClaudius = books.addBook("Robert Graves", "I, Claudius", "113 [2]");
 
-  IMember jim = members.addMember("Jim", "Johnson", "", "");
-  IMember sam = members.addMember("Sam", "Malone", "", "");
-  IMember jill = members.addMember("Jill", "Underhill", "", "");
-  IMember bob = members.addMember("Bob", "Dylan", "", "");
-  IMember eric = members.addMember("Eric", "Idle", "", "");
+  IMember jim = members.addMember("Jim", "Johnson", "999123", "jim@gmail.com");
+  IMember sam = members.addMember("Sam", "Malone", "888124", "sam@yahoo.com");
+  IMember jill = members.addMember("Jill", "Hill", "777125", "jill@gmail.com");
+  IMember bob = members.addMember("Bob", "Dylan", "666126", "bob@dylan.com");
+  IMember eric = members.addMember("Eric", "Idle", "555127", "eric@life.com");
+
+  public void initializeController()
+  {
+    loans = new LoanDAO(loanHelper);
+    controller = new library.BorrowUC_CTL(reader, scanner, printer, display,
+                                          books, loans, members);
+  }
+
+  public void systemReadyForConfirmLoans()
+  {
+    setPrivateState(controller, EBorrowState.CONFIRMING_LOANS);
+    // other stuff to set up displays etc
+  }
+
+
 
   //===========================================================================
   // Test setup of library
@@ -103,9 +128,9 @@ public class TestConfirmLoansOperation
   @Test
   public void initialStateOfLibrary()
   {
-    loans = new LoanDAO(loanHelper);
+    initializeController();
     assertThat(books.listBooks()).hasSize(13);
-    assertThat(members.listMembers()).hasSize(6);
+    assertThat(members.listMembers()).hasSize(5);
     assertThat(loans.listLoans()).isEmpty();
   }
 
@@ -113,7 +138,7 @@ public class TestConfirmLoansOperation
   @Test
   public void canAddLoans()
   {
-    loans = new LoanDAO(loanHelper);
+    initializeController();
     ILoan firstLoan = loans.createLoan(eric, iClaudius);
     ILoan secondLoan = loans.createLoan(bob, atonement);
     assertThat(loans.listLoans()).isEmpty();
@@ -123,19 +148,154 @@ public class TestConfirmLoansOperation
     assertThat(loans.listLoans()).hasSize(2);
   }
 
-  //===========================================================================
-  // ?
-  //===========================================================================
+
+  @Test
+  public void preConditionsAreMet()
+  {
+    initializeController();
+    systemReadyForConfirmLoans();
+
+    ILoan firstLoan = loans.createLoan(jim, catch22);
+    List<ILoan> pendingLoans = new ArrayList<>();
+    pendingLoans.add(firstLoan);
+    assertThat(loans.listLoans()).isEmpty();
+    assertThat(firstLoan.getID()).isEqualTo(0);
+    setPrivateLoanList(controller, pendingLoans);
+
+    // assert pre-conditions met
+    assertThat(controller.getClass()).isEqualTo(BorrowUC_CTL.class);
+    assertThat(getPrivateState(controller))
+        .isEqualTo(EBorrowState.CONFIRMING_LOANS);
+    assertThat(getPrivateLoanList(controller)).isNotEmpty();
+  }
 
 
   //===========================================================================
   // ?
   //===========================================================================
 
+  @Test
+  public void confirmLoan_OneBookHappyPath_ResultsCorrect()
+  {
+    initializeController();
+    systemReadyForConfirmLoans();
+
+    ILoan firstLoan = loans.createLoan(jim, catch22);
+    List<ILoan> pendingLoans = new ArrayList<>();
+    pendingLoans.add(firstLoan);
+    assertThat(loans.listLoans()).isEmpty();
+    assertThat(firstLoan.getID()).isEqualTo(0);
+    setPrivateLoanList(controller, pendingLoans);
+
+    controller.loansConfirmed();
+
+    assertThat(loans.listLoans()).hasSize(1);
+    assertThat(loans.getLoanByID(1).getBorrower()).isSameAs(jim);
+    assertThat(loans.getLoanByID(1).getBook()).isSameAs(catch22);
+    assertThat(loans.getLoanByID(1)).isSameAs(firstLoan);
+    assertThat(firstLoan.getID()).isEqualTo(1);
+  }
+
+
+  @Test
+  public void confirmLoan_ThreeBooksHappyPath_ResultsCorrect()
+  {
+    initializeController();
+    systemReadyForConfirmLoans();
+    IMember borrower = jim;
+    List<IBook> bookList = setUpBookList(catch22, emma, atonement);
+    List<ILoan> pendingLoans = setUpPendingLoans(borrower, bookList);
+    Date today = ignoreTime(new Date());
+    Date due = calculateDueDate(today);
+    setPrivateLoanList(controller, pendingLoans);
+
+    controller.loansConfirmed();
+
+    ILoan firstLoan = loans.getLoanByID(1);
+    ILoan secondLoan = loans.getLoanByID(2);
+    ILoan thirdLoan = loans.getLoanByID(3);
+
+    assertThat(loans.listLoans()).hasSize(3);
+    assertThat(firstLoan.getBorrower()).isSameAs(jim);
+    assertThat(secondLoan.getBorrower()).isSameAs(jim);
+    assertThat(thirdLoan.getBorrower()).isSameAs(jim);
+    assertThat(firstLoan.getBook()).isSameAs(catch22);
+    assertThat(secondLoan.getBook()).isSameAs(emma);
+    assertThat(thirdLoan.getBook()).isSameAs(atonement);
+    Date borrowDate = getPrivateBorrowDate((Loan)firstLoan);
+    assertThat(borrowDate).isEqualTo(today);
+    borrowDate = getPrivateBorrowDate((Loan)secondLoan);
+    assertThat(borrowDate).isEqualTo(today);
+    borrowDate = getPrivateBorrowDate((Loan)thirdLoan);
+    assertThat(borrowDate).isEqualTo(today);
+
+    Date dueDate = getPrivateDueDate((Loan)firstLoan);
+    assertThat(dueDate).isEqualTo(calculateDueDate(today));
+    dueDate = getPrivateDueDate((Loan)secondLoan);
+    assertThat(dueDate).isEqualTo(calculateDueDate(today));
+    dueDate = getPrivateDueDate((Loan)thirdLoan);
+    assertThat(dueDate).isEqualTo(calculateDueDate(today));
+    assertThat(loans.findLoansByBorrower(jim)).containsExactly(firstLoan,
+                                                               secondLoan,
+                                                               thirdLoan);
+    assertThat(loans.listLoans()).containsExactly(firstLoan,
+                                                  secondLoan,
+                                                  thirdLoan);
+
+  }
 
   //===========================================================================
   // ?
   //===========================================================================
 
+
+  //===========================================================================
+  // Loan helper
+  //===========================================================================
+
+  private List<ILoan> setUpPendingLoans(IMember borrower,
+                                        List<IBook> bookList)
+  {
+    List<ILoan> pendingLoans = new ArrayList<>();
+    for (IBook book : bookList) {
+      ILoan loan = loans.createLoan(borrower, book);
+      pendingLoans.add(loan);
+    }
+    return pendingLoans;
+  }
+
+
+
+  private List<IBook> setUpBookList(IBook... booksToBeBorrowed)
+  {
+    List<IBook> bookList = new ArrayList<>();
+    for (IBook book : booksToBeBorrowed) {
+      bookList.add(book);
+    }
+    return bookList;
+  }
+
+
+
+  private Date ignoreTime(Date date)
+  {
+    Calendar calendar = Calendar.getInstance();
+
+    calendar.setTime(date);
+    calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);
+    calendar.set(java.util.Calendar.MINUTE, 0);
+    calendar.set(java.util.Calendar.SECOND, 0);
+    calendar.set(java.util.Calendar.MILLISECOND, 0);
+
+    return calendar.getTime();
+  }
+
+  private Date calculateDueDate(Date date)
+  {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(date);
+    calendar.add(Calendar.DAY_OF_MONTH, ILoan.LOAN_PERIOD);
+    return calendar.getTime();
+  }
 
 }
