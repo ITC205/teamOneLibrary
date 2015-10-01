1ff23832588b2449026b22ba1a788fbb52f21bb8
Josh <josh-kent@hotmail.com>
2015-10-01 16:41:09 +1000

    TestUAT class ready to use

--- src/test/scenario/TestUATJosh.java
+++ src/test/scenario/TestUATJosh.java
@@ -1,10 +1,6 @@
 package test.scenario;
 
-import java.util.Calendar;
-import java.util.Date;
-
 import library.BorrowUC_CTL;
-import library.Main;
 import library.daos.BookDAO;
 import library.daos.BookHelper;
 import library.daos.LoanDAO;
@@ -30,15 +26,20 @@ import library.panels.MainPanel;
 /**
  * TestUATJosh class
  * 
- * This class is provided as an alternative for executing the 'Main' class, 
+ * This class is provided as an alternative for executing the 'Main' class and is 
  * specifically for users performing UAT testing. This class simulates the 'Main'
  * class and uses all real object and UIs. The only point of difference is that 
  * this class will ensure users carrying out UAT test scripts have the correct 
- * test data available to them. Most of 'Main' has been reproduced here, the 
- * reason that 'Main' wasn't used directly was due to the setupTestData() call 
- * in the 'Main' constructor that would create alternative test data. It is 
- * convenient to keep the original 'Main' class as it is, so that my UAT testing 
- * does not interfere with other members of the group.  
+ * test data available to them. 
+ * 
+ * Most of 'Main' has been reproduced here; the reason that 'Main' wasn't used 
+ * directly was due to the setupTestData() call in the 'Main' constructor that 
+ * would create test data not relevant to my UATS. 
+ * 
+ * Hence, the only difference between this class and 'Main' is the implementation 
+ * of the setupTestData() method. It is convenient to keep the original 'Main' 
+ * class as it is, so that my UAT testing does not interfere with other members 
+ * of the group.  
  * 
  * @author Josh Kent
  */
@@ -109,7 +110,33 @@ public class TestUATJosh
 
   
   private void setupTestData() {
-
+    // Add books ==============================================================
+    IBook disposedBook = bookDAO.addBook("Harper Lee", "To Kill A Mockingbird", "LEE 123");
+    bookDAO.addBook("Harper Lee", "Go Set a Watchman", "LEE 445");
+    bookDAO.addBook("Dan Brown", "Digital Fortress", "BRO 410");
+    bookDAO.addBook("Tom Grace", "Quantum", "GRA 888");
+    
+    IBook loanBookOne = bookDAO.addBook("Dean Crawford", "Immortal", "CRA 111");
+    IBook loanBookTwo = bookDAO.addBook("Stephen King", "It", "KIN 109");
+    
+    // Add members ============================================================
+    IMember memberOne = memberDAO.addMember("Josh", "Kent", "62626262", "josh@kent.com");
+    memberDAO.addMember("John", "Kent", "63636363", "john@kent.com");
+    memberDAO.addMember("Jason", "Kent", "64646464", "jason@kent.com");
+    
+    // Setup Member Conditions =================================================
+    // Two loans added to memberOne
+    ILoan loanOne = loanDAO.createLoan(memberOne, loanBookOne);
+    loanDAO.commitLoan(loanOne);
+    ILoan loanTwo = loanDAO.createLoan(memberOne, loanBookTwo);
+    loanDAO.commitLoan(loanTwo);
+  
+    // Add $3.00 fine to memberOne
+    memberOne.addFine(3.0F);
+    
+    // Setup Book Conditions ==================================================
+    disposedBook.dispose();
+    
   }
 
   
