44cf9215544a7c8dd5f20aaf90399e084b1b39c6
nickbaldwin <nick.with.a.z@gmail.com>
2015-10-02 18:11:27 +1000

    + suites

--- src/test/suite/TestOperationsSuiteNick.java
+++ src/test/suite/TestOperationsSuiteNick.java
@@ -1,38 +1,22 @@
 package test.suite;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
 /**
+ * Groups test classes to be run as a suite.
  *
+ * @author nicholasbaldwin
  */
+@RunWith(org.junit.runners.Suite.class)
+@SuiteClasses(
+  {
+    test.integration.TestConfirmLoansOperation.class,
+    test.integration.TestRejectLoansOperation.class
+  })
+
 public class TestOperationsSuiteNick
 {
-  //===========================================================================
-  // Variables
-  //===========================================================================
-
-
-  //===========================================================================
-  // Constructors
-  //===========================================================================
-
-  private TestOperationsSuiteNick()
-
-  {
-  }
-
-
-  //===========================================================================
-  // Primary methods
-  //===========================================================================
-
-
-  //===========================================================================
-  // Helper methods
-  //===========================================================================
-
-
-  //===========================================================================
-  // Getters & setters
-  //===========================================================================
-
-
+ /* empty class */
 }
