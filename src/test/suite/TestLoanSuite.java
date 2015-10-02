01ffdf96818e249620f692d5402fb29791b09ff8
nickbaldwin <nick.with.a.z@gmail.com>
2015-10-02 17:31:35 +1000

    added test suite for all 3 families

--- src/test/suite/TestLoanSuite.java
+++ src/test/suite/TestLoanSuite.java
@@ -12,13 +12,11 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(org.junit.runners.Suite.class)
 @SuiteClasses(
   {
-    test.unit.TestLoan.class,
-    test.unit.TestLoanHelper.class,
-    test.unit.TestLoanDAO.class,
+    test.suite.TestSuiteLoanFamilyUnit.class,
     test.collaboration.TestLoanFamily.class
   })
 
-public class TestSuiteLoanFamily
+public class TestLoanSuite
 {
  /* empty class */
 }
